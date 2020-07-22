ac0cd145cac0e8ad923dde3cc020b6557d649727
v-caoyongqi <v-caoyongqi@xiaomi.com>
2020-07-22 13:26:22 +0800

    del data

--- _data/app/main/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-#-*- coding:utf-8 -*-
-# author:muji
-# datetime:2019/8/1 13:46
-from flask import Blueprint
-
-web = Blueprint('web', __name__)
-
-from app.main.views import index, cases, modules, projects, suites, ajax_view, user
--- _data/app/main/views/__init__.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-#-*- coding:utf-8 -*-
-# author:muji
-# datetime:2019/8/1 17:19
